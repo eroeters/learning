6ff6fdc3216db9c4aea9d4a82140c571e93615bd
Eric R <erroeters@gmail.com>
2023-11-07 21:15:50 -0500

    Work on JS
    Continued work on Odin project JS fundamentals

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,13 @@
-console.log(123e5);
+console.log(35 + 48);
 
-let a = 1,
-  b = 1;
-let c = ++a;
-let d = b++;
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
