1725cd5ed97e658ea1d7d419520d763de3217abc
Eric R <erroeters@gmail.com>
2023-11-15 16:42:49 -0500

    Add functions 2 completed files
    Added functions2 files with completed js for mdn tutorial

--- javascript/functions2/functions2.js
+++ javascript/functions2/functions2.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const button = document.querySelector("button");
 
 const x = 50;
 const y = 60;
@@ -9,6 +10,15 @@ const color = "blue";
 
 // Add your code here
 
+button.addEventListener("click", () => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+});
+
+// function draw() {
+//   ctx.fillStyle = color;
+//   ctx.fillRect(x, y, width, height);
+// }
 // Don't edit the code below here!
 
 const section = document.querySelector("section");
