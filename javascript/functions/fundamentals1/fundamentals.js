c133128b5b7fc3a190a5ab9b1181e4c12037fe4d
Eric R <erroeters@gmail.com>
2023-11-20 15:54:22 -0500

    Add completed multiply function
    Added completed multiply function to fundamentals folder

--- javascript/functions/fundamentals1/fundamentals.js
+++ javascript/functions/fundamentals1/fundamentals.js
@@ -1,6 +1,12 @@
 const addNum = document.querySelector("#addNum");
 const addNumTxt = document.querySelector("#addNumTxt");
+const mult1 = document.querySelector("#mult1");
+const mult2 = document.querySelector("#mult2");
 
 function add() {
   addNumTxt.innerText = Number(addNum.value) + 7;
+}
+
+function multiply() {
+  multNumTxt.innerText = Number(mult1.value) * Number(mult2.value);
 }
