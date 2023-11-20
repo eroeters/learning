ec7da38ac1db5ab248491a795b408b11ce970c42
Eric R <erroeters@gmail.com>
2023-11-20 16:37:26 -0500

    Add capitalize function to folder
    Added completed capitalize function to folder

--- javascript/functions/fundamentals1/fundamentals.js
+++ javascript/functions/fundamentals1/fundamentals.js
@@ -1,7 +1,10 @@
 const addNum = document.querySelector("#addNum");
 const addNumTxt = document.querySelector("#addNumTxt");
+
 const mult1 = document.querySelector("#mult1");
 const mult2 = document.querySelector("#mult2");
+
+const cap = document.querySelector("#capitalize");
 
 function add() {
   addNumTxt.innerText = Number(addNum.value) + 7;
@@ -9,4 +12,9 @@ function add() {
 
 function multiply() {
   multNumTxt.innerText = Number(mult1.value) * Number(mult2.value);
+}
+
+function capitalize() {
+  let string = cap.value;
+  capTxt.innerText = string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
