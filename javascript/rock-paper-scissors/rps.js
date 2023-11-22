05dc9f73769cf2ff92d42c92abdcf5d7d5f6fed5
Eric R <erroeters@gmail.com>
2023-11-21 21:10:19 -0500

    Finish playGame function
    Finished playGame function and debugged issues. Working

--- javascript/rock-paper-scissors/rps.js
+++ javascript/rock-paper-scissors/rps.js
@@ -1,19 +1,18 @@
-console.log(choice);
-
 function getComputerChoice() {
   let selection = Math.floor(Math.random() * 3) + 1;
   if (selection == 1) {
-    console.log(selection);
+    // console.log(selection);
     return selection;
   } else if (selection == 2) {
-    console.log(selection);
+    // console.log(selection);
     return selection;
   } else if (selection == 3) {
-    console.log(selection);
+    // console.log(selection);
     return selection;
   }
 }
 function playGame() {
+  getComputerChoice();
   let choice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
   if (choice == "rock") {
     if (getComputerChoice() == 1) {
@@ -22,6 +21,24 @@ function playGame() {
       console.log("You lose! Paper covers rock");
     } else if (getComputerChoice() == 3) {
       console.log("You win! Rock smashes scissors");
+    }
+  } else if (choice == "paper") {
+    if (getComputerChoice() == 1) {
+      console.log("The computer chose rock, you win! Paper covers rock");
+    } else if (getComputerChoice() == 2) {
+      console.log("The computer chose paper, you tied!");
+    } else if (getComputerChoice() == 3) {
+      console.log("The computer chose scissors, you lose! Scissors cuts paper");
+    }
+  } else if (choice == "scissors") {
+    if (getComputerChoice() == 1) {
+      console.log(
+        "The computer chose rock, you win! You lose! Rock smashes scissors"
+      );
+    } else if (getComputerChoice() == 2) {
+      console.log("The computer chose paper, you win! Scissors cuts paper");
+    } else if (getComputerChoice() == 3) {
+      console.log("The computer chose scissors, you tied!");
     }
   }
 }
