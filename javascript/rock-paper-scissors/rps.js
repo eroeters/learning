68f7b69407de78c5404967f0fe4e7c7bca6fed24
Eric R <erroeters@gmail.com>
2023-11-21 21:29:47 -0500

    Change console.log into changing innerText of HTML
    Changed console.log outputs into changing innerText of playTxt span

--- javascript/rock-paper-scissors/rps.js
+++ javascript/rock-paper-scissors/rps.js
@@ -1,3 +1,10 @@
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const playTxt = document.querySelector("#playTxt");
+
+// Random generator for computer choice
+// Gets called in playGame function after player clicks Play button
+
 function getComputerChoice() {
   let selection = Math.floor(Math.random() * 3) + 1;
   if (selection == 1) {
@@ -8,36 +15,41 @@ function getComputerChoice() {
     return selection;
   }
 }
+
+// Takes value of player input and matches it against getComputerChoice function
+
 function playGame() {
-  getComputerChoice();
+  let playerScore = 0;
+  let computerScore = 0;
   let choice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
   if (choice == "rock") {
     if (getComputerChoice() == 1) {
-      console.log("The computer chose rock, you tied!");
+      playTxt.innerText = "The computer chose rock, you tied!";
     } else if (getComputerChoice() == 2) {
-      console.log("THe computer chose paper, you lose! Paper covers rock");
+      playTxt.innerText =
+        "The computer chose paper, you lose! Paper covers rock";
     } else if (getComputerChoice() == 3) {
-      console.log(
-        "The computer chose scissors, you win! Rock smashes scissors"
-      );
+      playTxt.innerText =
+        "The computer chose scissors, you win! Rock smashes scissors";
     }
   } else if (choice == "paper") {
     if (getComputerChoice() == 1) {
-      console.log("The computer chose rock, you win! Paper covers rock");
+      playTxt.innerText = "The computer chose rock, you win! Paper covers rock";
     } else if (getComputerChoice() == 2) {
-      console.log("The computer chose paper, you tied!");
+      playTxt.innerText = "The computer chose paper, you tied!";
     } else if (getComputerChoice() == 3) {
-      console.log("The computer chose scissors, you lose! Scissors cuts paper");
+      playTxt.innerText =
+        "The computer chose scissors, you lose! Scissors cuts paper";
     }
   } else if (choice == "scissors") {
     if (getComputerChoice() == 1) {
-      console.log(
-        "The computer chose rock, you win! You lose! Rock smashes scissors"
-      );
+      playTxt.innerText =
+        "The computer chose rock, you lose! Rock smashes scissors";
     } else if (getComputerChoice() == 2) {
-      console.log("The computer chose paper, you win! Scissors cuts paper");
+      playTxt.innerText =
+        "The computer chose paper, you win! Scissors cuts paper";
     } else if (getComputerChoice() == 3) {
-      console.log("The computer chose scissors, you tied!");
+      playTxt.innerText = "The computer chose scissors, you tied!";
     }
   }
 }
