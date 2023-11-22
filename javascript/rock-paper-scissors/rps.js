aea40fe0d4bc1f9dd29e8f0c2c0ca5b1fdcb6ccb
Eric R <erroeters@gmail.com>
2023-11-21 21:14:49 -0500

    Update output on rock choice
    Updated console log if player chooses rock

--- javascript/rock-paper-scissors/rps.js
+++ javascript/rock-paper-scissors/rps.js
@@ -1,13 +1,10 @@
 function getComputerChoice() {
   let selection = Math.floor(Math.random() * 3) + 1;
   if (selection == 1) {
-    // console.log(selection);
     return selection;
   } else if (selection == 2) {
-    // console.log(selection);
     return selection;
   } else if (selection == 3) {
-    // console.log(selection);
     return selection;
   }
 }
@@ -16,11 +13,13 @@ function playGame() {
   let choice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
   if (choice == "rock") {
     if (getComputerChoice() == 1) {
-      console.log("You tied!");
+      console.log("The computer chose rock, you tied!");
     } else if (getComputerChoice() == 2) {
-      console.log("You lose! Paper covers rock");
+      console.log("THe computer chose paper, you lose! Paper covers rock");
     } else if (getComputerChoice() == 3) {
-      console.log("You win! Rock smashes scissors");
+      console.log(
+        "The computer chose scissors, you win! Rock smashes scissors"
+      );
     }
   } else if (choice == "paper") {
     if (getComputerChoice() == 1) {
