863c4212b5b3efce586f25209b03d577e451ee69
Eric R <erroeters@gmail.com>
2023-11-28 16:52:23 -0500

    Begin work on js exercise 4

--- learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, value) {
+  let index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
