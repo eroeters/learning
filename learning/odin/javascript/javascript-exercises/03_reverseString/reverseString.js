73129ee788c77b57e4f9f8a6802447912f16eab7
Eric R <erroeters@gmail.com>
2023-11-28 15:15:13 -0500

    Complete test 03 in js exercises

--- learning/odin/javascript/javascript-exercises/03_reverseString/reverseString.js
+++ learning/odin/javascript/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let split = str.split("");
+  let reverse = split.reverse();
+  let join = split.join("");
+  return join;
 };
 
 // Do not edit below this line
