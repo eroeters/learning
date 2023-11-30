7cbf86416796797c011bd544a252f110086933bc
Eric R <erroeters@gmail.com>
2023-11-30 17:00:36 -0500

    Practice rest functions

--- learning/odin/javascript/functions/jsTestFiles/test1/test1.js
+++ learning/odin/javascript/functions/jsTestFiles/test1/test1.js
@@ -5,3 +5,27 @@ const p = document.querySelector("p");
 function pressMe() {
   p.innerText = `Hello ${firstName.value} ${lastName.value}!`;
 }
+
+function sum(...args) {
+  let total = 0;
+  for (const arg of args) {
+    total += arg;
+  }
+  return total;
+}
+
+function mult(...args) {
+  let total = 1;
+  for (const arg of args) {
+    total *= arg;
+  }
+  return total;
+}
+
+function div(...args) {
+  let total = 1;
+  for (const arg of args) {
+    total /= arg;
+  }
+  return total;
+}
--- learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,10 +1,9 @@
-const removeFromArray = function (arr, value) {
-  let index = arr.indexOf(value);
+const removeFromArray = function (arr, ...args) {
+  var index = arr.indexOf(value);
   if (index > -1) {
     arr.splice(index, 1);
   }
   return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
