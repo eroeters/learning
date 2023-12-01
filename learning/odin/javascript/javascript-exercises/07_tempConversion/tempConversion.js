a70e486147e3d0301ec736d4266eb24b8f63f060
Eric R <erroeters@gmail.com>
2023-12-01 17:04:43 -0500

    Start js exercise 7
    Started js exercise 7, functions working but need to work on non-decimal integers showing with toFixed method

--- learning/odin/javascript/javascript-exercises/07_tempConversion/tempConversion.js
+++ learning/odin/javascript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let cels = (temp - 32) * (5 / 9);
+  if (cels > 0 || cels < 0) {
+    return cels.toFixed(1);
+  } else {
+    return cels;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahren = temp * (9 / 5) + 32;
+  if (fahren > 0 || fahren < 0) {
+    return fahren.toFixed(1);
+  } else {
+    return fahren;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
