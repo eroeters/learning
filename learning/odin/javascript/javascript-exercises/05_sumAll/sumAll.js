fc9d58db6545a0969aaf696418cb817730ee7252
Eric R <erroeters@gmail.com>
2023-12-01 16:25:13 -0500

    Finish js exercise 5

--- learning/odin/javascript/javascript-exercises/05_sumAll/sumAll.js
+++ learning/odin/javascript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  // checks to see if arguments are integers
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (a > b) {
+    for (let i = a; i >= b; i--) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
