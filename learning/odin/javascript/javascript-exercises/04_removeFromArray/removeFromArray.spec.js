209045a5bda5c3f0ed1149438fcea5c49e751c09
Eric R <erroeters@gmail.com>
2023-12-01 15:54:02 -0500

    Finish javascript exercise 4

--- learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.spec.js
+++ learning/odin/javascript/javascript-exercises/04_removeFromArray/removeFromArray.spec.js
@@ -13,13 +13,13 @@ describe("removeFromArray", () => {
   test("ignores non present values, but still works", () => {
     expect(removeFromArray([1, 2, 3, 4], 7, 2)).toEqual([1, 3, 4]);
   });
-  test.skip("can remove all values", () => {
+  test("can remove all values", () => {
     expect(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)).toEqual([]);
   });
-  test.skip("works with strings", () => {
+  test("works with strings", () => {
     expect(removeFromArray(["hey", 2, 3, "ho"], "hey", 3)).toEqual([2, "ho"]);
   });
-  test.skip("only removes same type", () => {
+  test("only removes same type", () => {
     expect(removeFromArray([1, 2, 3], "1", 3)).toEqual([1, 2]);
   });
 });
--- learning/odin/javascript/javascript-exercises/04_removeFromArray/solution/removeFromArray-solution.js
+++ learning/odin/javascript/javascript-exercises/04_removeFromArray/solution/removeFromArray-solution.js
@@ -5,12 +5,12 @@ const removeFromArray = function (array, ...args) {
   // create a new empty array
   const newArray = [];
   // use forEach to go through the array
-  array.forEach((item) => {
+  array.forEach((arg) => {
     // push every element into the new array
     // UNLESS it is included in the function arguments
     // so we create a new array with every item, except those that should be removed
-    if (!args.includes(item)) {
-      newArray.push(item);
+    if (!args.includes(arg)) {
+      newArray.push(arg);
     }
   });
   // and return that array
