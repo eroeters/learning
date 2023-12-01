0fe7cd239c31aec0b8d49dee3bf631dd69efd8c6
Eric R <erroeters@gmail.com>
2023-12-01 16:53:11 -0500

    Finish js exercise 6

--- learning/odin/javascript/javascript-exercises/06_leapYears/leapYears.js
+++ learning/odin/javascript/javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,27 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  //  working function that I made before solution
 
+  //   if (year % 4 === 0) {
+  //     if (year % 100 === 0) {
+  //       if (year % 400 === 0) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     } else {
+  //       return true;
+  //     }
+  //   } else {
+  //     return false;
+  //   }
+
+  //   working function after checking solution
+
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
