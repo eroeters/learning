dedbf5953b3f56cafe77609d8fa58bbb72ece0b1
Eric R <erroeters@gmail.com>
2023-12-04 15:18:45 -0500

    Finish js exercise 7
    Finished js exercise 7, switched returns from regular toFixed to parseFloat(num.toFixed) to pass tests

--- learning/odin/javascript/javascript-exercises/07_tempConversion/tempConversion.js
+++ learning/odin/javascript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,7 @@
 const convertToCelsius = function (temp) {
   let cels = (temp - 32) * (5 / 9);
   if (cels > 0 || cels < 0) {
-    return cels.toFixed(1);
+    return parseFloat(cels.toFixed(1));
   } else {
     return cels;
   }
@@ -10,7 +10,7 @@ const convertToCelsius = function (temp) {
 const convertToFahrenheit = function (temp) {
   let fahren = temp * (9 / 5) + 32;
   if (fahren > 0 || fahren < 0) {
-    return fahren.toFixed(1);
+    return parseFloat(fahren.toFixed(1));
   } else {
     return fahren;
   }
