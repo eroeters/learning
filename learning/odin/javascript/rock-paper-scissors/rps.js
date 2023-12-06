8a5e72709775a9733af786d3e0fd9a7fcc32836f
Eric R <erroeters@gmail.com>
2023-12-06 15:39:01 -0500

    Complete RPS game
    Completed working RPS game update with GUI based on Odin assignment

--- learning/odin/javascript/rock-paper-scissors/rps.js
+++ learning/odin/javascript/rock-paper-scissors/rps.js
@@ -4,17 +4,17 @@
 
 // // random number function to determine computer choice
 
-function getComputerChoice() {
-  let choice = Math.floor(Math.random() * 3);
-  switch (choice) {
-    case 0:
-      return rock;
-    case 1:
-      return paper;
-    case 2:
-      return scissors;
-  }
-}
+// function getComputerChoice() {
+//   let choice = Math.floor(Math.random() * 3);
+//   switch (choice) {
+//     case 0:
+//       return rock;
+//     case 1:
+//       return paper;
+//     case 2:
+//       return scissors;
+//   }
+// }
 
 // switch function to determine use cases for player and computer choices
 
@@ -44,23 +44,90 @@ function getComputerChoice() {
 
 // New event listener function for game
 
+// function getComputerChoice() {
+//   return Math.floor(Math.random() * 3);
+// }
+
 const buttons = document.querySelectorAll("button");
+const playerScoreText = document.querySelector("#playerScore");
+const cpuScoreText = document.querySelector("#computerScore");
+const roundMessage = document.querySelector("#roundMessage");
+const numRounds = document.querySelector("#rounds");
+
+let playerScore = 0;
+let cpuScore = 0;
+let rounds = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (button.id == "rock") {
-      if (getComputerChoice == "rock") {
-        alert("you tied");
-      } else if (getComputerChoice == "paper") {
-        alert("The CPU chose paper, you lose!");
-      } else if (getComputerChoice == "scissors") {
-        alert("The CPU chose scissors, you win!");
+    let cpuChoice = Math.floor(Math.random() * 3);
+    if (playerScore == 5 || cpuScore == 5) {
+      if (playerScore == 5) {
+        alert(`You have won the game in ${rounds} rounds!`);
+      } else if (cpuScore == 5) {
+        alert(`The computer has won the game in ${rounds} rounds!`);
       }
-      // alert("You pressed the 1st button");
-    } else if (button.id == "paper") {
-      alert("You pressed the 2nd button");
-    } else if (button.id == "scissors") {
-      alert("You pressed the 3rd button");
+    } else {
+      // rock
+      if (button.id == "rock") {
+        if (cpuChoice == 0) {
+          rounds++;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The computer also chose Rock, you tied!";
+        } else if (cpuChoice == 1) {
+          rounds++;
+          cpuScore++;
+          cpuScoreText.textContent = cpuScore;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The CPU chose paper, you lose!";
+        } else if (cpuChoice == 2) {
+          rounds++;
+          playerScore++;
+          playerScoreText.textContent = playerScore;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The CPU chose scissors, you win!";
+        }
+      }
+      // paper
+      else if (button.id == "paper") {
+        if (cpuChoice == 0) {
+          rounds++;
+          playerScore++;
+          playerScoreText.textContent = playerScore;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The computer chose rock, you win!";
+        } else if (cpuChoice == 1) {
+          rounds++;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The CPU chose paper, you tied!";
+        } else if (cpuChoice == 2) {
+          rounds++;
+          cpuScore++;
+          cpuScoreText.textContent = cpuScore;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The CPU chose scissors, you lose!";
+        }
+      }
+      // scissors
+      else if (button.id == "scissors") {
+        if (cpuChoice == 0) {
+          rounds++;
+          cpuScore++;
+          numRounds.textContent = rounds;
+          cpuScoreText.textContent = cpuScore;
+          roundMessage.textContent = "The computer chose rock, you lose!";
+        } else if (cpuChoice == 1) {
+          rounds++;
+          playerScore++;
+          numRounds.textContent = rounds;
+          playerScoreText.textContent = playerScore;
+          roundMessage.textContent = "The CPU chose paper, you win!";
+        } else if (cpuChoice == 2) {
+          rounds++;
+          numRounds.textContent = rounds;
+          roundMessage.textContent = "The CPU chose scissors, you tied!";
+        }
+      }
     }
   });
 });
