b059e3be38c60c261b1a9c7bdce3f74c466b60a6
Eric R <erroeters@gmail.com>
2023-12-06 16:30:24 -0500

    Change for loop
    Changed for loop as the loop was creating 17 boxes, not 16

--- learning/odin/javascript/etch-a-sketch/etchgame.js
+++ learning/odin/javascript/etch-a-sketch/etchgame.js
@@ -1,6 +1,6 @@
 const container = document.querySelector("#gameContainer");
 
-for (let i = 0; i <= 16; i++) {
+for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
   div.classList.add("box");
   container.appendChild(div);
