2c0c9e87ee78943213424316c032a06ba7ef88f1
Eric R <erroeters@gmail.com>
2023-12-06 16:57:10 -0500

    Add gridSize const
    Added gridSize constant to prompt user to enter number and turn value into number

--- learning/odin/javascript/etch-a-sketch/etchgame.js
+++ learning/odin/javascript/etch-a-sketch/etchgame.js
@@ -3,6 +3,8 @@ const generateGrid = document.querySelector("#generateGrid");
 
 generateGrid.addEventListener("click", () => {
   container.textContent = "";
+  const gridSize = Number(window.prompt("How many boxes would you like?"));
+  console.log(gridSize);
   for (let i = 0; i < 16; i++) {
     const div = document.createElement("div");
     div.classList.add("box");
