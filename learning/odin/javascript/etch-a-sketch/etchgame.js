07870ea5455c24ae2beb2d80e54513a83111bdb0
Eric R <erroeters@gmail.com>
2023-12-07 14:35:11 -0500

    Change loop to add multiple rows
    Changed onclick function to add rows based on input and each row to create box divs based on input

--- learning/odin/javascript/etch-a-sketch/etchgame.js
+++ learning/odin/javascript/etch-a-sketch/etchgame.js
@@ -6,7 +6,11 @@ generateGrid.addEventListener("click", () => {
   const gridSize = Number(window.prompt("How many boxes would you like?"));
   for (let i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
-    div.classList.add("box");
     container.appendChild(div);
+    for (let i = 0; i < gridSize; i++) {
+      const etchBox = document.createElement("div");
+      etchBox.classList.add("box");
+      div.appendChild(etchBox);
+    }
   }
 });
