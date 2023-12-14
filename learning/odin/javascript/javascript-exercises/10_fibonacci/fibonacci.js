1dc5026d72d9d52726d150527f170873fdc1aeed
Eric R <erroeters@gmail.com>
2023-12-14 16:37:28 -0500

    Start work in js exercise 10

--- learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  // create array
+  let array = [1];
+  let sequence = 1;
+  for (let i = 1; i <= number; i++) {
+    sequence += i;
+    array.push(sequence);
+    console.log(array);
+  }
+  return array[number];
 };
 
 // Do not edit below this line
