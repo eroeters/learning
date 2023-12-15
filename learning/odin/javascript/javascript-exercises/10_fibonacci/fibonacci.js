f5cb0846671bf47820f7b135afb35cfbe13c573b
Eric R <erroeters@gmail.com>
2023-12-15 14:23:49 -0500

    Finish fibonacci function, seems to work for now

--- learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,13 +1,14 @@
 const fibonacci = function (number) {
-  // create array
   let array = [1];
-  let sequence = 1;
-  for (let i = 1; i <= number; i++) {
-    sequence += i;
-    array.push(sequence);
+  if (number < 0) {
+    return "OOPS"
+  }
+  for (let i = 0, j = 1, k = 0; k < number; i = j, j = x, k++) {
+    x = i + j;
+    array.push(x);
     console.log(array);
   }
-  return array[number];
+  return Number.(array[number - 1]);
 };
 
 // Do not edit below this line
