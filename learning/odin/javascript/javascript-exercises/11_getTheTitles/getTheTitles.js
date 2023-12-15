326e4202d6b19df6f4de5b41e9e476d553911be0
Eric R <erroeters@gmail.com>
2023-12-15 14:41:54 -0500

    Finish js exercise 11 and add comments

--- learning/odin/javascript/javascript-exercises/11_getTheTitles/getTheTitles.js
+++ learning/odin/javascript/javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  // create initial array
+  let bookArray = [];
+  //   for...of loop to iterate through given arrays
+  for (book of books) {
+    // push book title to array
+    bookArray.push(book.title);
+  }
+  return bookArray;
 };
 
 // Do not edit below this line
