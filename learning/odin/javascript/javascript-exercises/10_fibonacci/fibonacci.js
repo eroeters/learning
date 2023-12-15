4ed3c521dd1675d2fd8837a00b986ce298486e89
Eric R <erroeters@gmail.com>
2023-12-15 14:25:12 -0500

    Finish js exercise 10

--- learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ learning/odin/javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,14 +1,14 @@
 const fibonacci = function (number) {
   let array = [1];
   if (number < 0) {
-    return "OOPS"
+    return "OOPS";
   }
   for (let i = 0, j = 1, k = 0; k < number; i = j, j = x, k++) {
     x = i + j;
     array.push(x);
     console.log(array);
   }
-  return Number.(array[number - 1]);
+  return Number(array[number - 1]);
 };
 
 // Do not edit below this line
