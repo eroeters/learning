18e352632d46339544fab1bddd5d8da853a09bdd
Eric R <erroeters@gmail.com>
2023-12-18 15:49:52 -0500

    Add HTML skeleton, basic js and css

---
+++ learning/odin/repos/calculator/calculator.js
@@ -0,0 +1,21 @@
+const addBtn = document.querySelector("#addBtn");
+const subBtn = document.querySelector("#subBtn");
+const multBtn = document.querySelector("#multBtn");
+const divideBtn = document.querySelector("#divideBtn");
+const displayText = document.querySelector("#displayText");
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
