9e98323457788b66d709498f8dd3636d4fd709cd
Eric R <erroeters@gmail.com>
2023-12-18 16:14:34 -0500

    Add functions for numberBtn and operatorBtn

--- learning/odin/repos/calculator/calculator.js
+++ learning/odin/repos/calculator/calculator.js
@@ -3,6 +3,8 @@ const subBtn = document.querySelector("#subBtn");
 const multBtn = document.querySelector("#multBtn");
 const divideBtn = document.querySelector("#divideBtn");
 const displayText = document.querySelector("#displayText");
+const numberBtn = document.querySelector(".numberBtn");
+const operatorBtn = document.querySelector(".operator");
 
 function sum(a, b) {
   return a + b;
@@ -18,4 +20,25 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+let storedNumber = "";
+
+numberBtn.forEach((number) => {
+  number.addEventListener("click", function () {
+    storedNumber += number.value;
+  });
+});
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
 }
