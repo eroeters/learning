ba448e997be731df884e34399dd766b7f13fc550
Eric R <erroeters@gmail.com>
2024-01-04 16:53:41 -0500

    Start work on custom form

--- learning/odin/html/forms/forms.js
+++ learning/odin/html/forms/forms.js
@@ -1,7 +1,7 @@
-const button = document.querySelector(".button");
-const output = document.querySelector("#submitText");
-const name = document.querySelector("#first_name");
+// const button = document.querySelector(".button");
+// const output = document.querySelector("#submitText");
+// const name = document.querySelector("#first_name");
 
-button.addEventListener("click", () => {
-  output.innerText = `Hello ${name.value}!`;
-});
+// button.addEventListener("click", () => {
+//   output.innerText = `Hello ${name.value}!`;
+// });
