9539c93fe6fc7942b25eae677c602c554412f44a
Eric R <erroeters@gmail.com>
2024-01-04 14:58:01 -0500

    Finish custom property code along

---
+++ learning/odin/css/custom-properties/custom-prop.js
@@ -0,0 +1,9 @@
+// function setTheme() {
+//   const root = document.documentElement;
+//   const newTheme = root.className === "dark" ? "light" : "dark";
+//   root.className = newTheme;
+
+//   document.querySelector(".theme-name").textContent = newTheme;
+// }
+
+// document.querySelector(".theme-toggle").addEventListener("click", setTheme);
