65a4b07efb5322255b0890f10983d211d726df6b
Eric R <erroeters@gmail.com>
2024-01-10 16:31:09 -0500

    Changed name of repos folder to projects

--- learning/odin/projects/sign-up/sign-up.js
+++ learning/odin/projects/sign-up/sign-up.js
@@ -2,8 +2,9 @@ const button = document.querySelector(".create-account-btn");
 const password = document.querySelector("#password");
 const confirmPass = document.querySelector("#confirm-pass");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
   if (password.value !== confirmPass.value) {
+    event.preventDefault();
     alert("Passwords must match");
   }
 });
