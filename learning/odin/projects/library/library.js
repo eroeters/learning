ef85216af546a25016227435cd8b8623f716217c
Eric R <erroeters@gmail.com>
2024-02-01 16:59:26 -0500

    Start work on refactoring addBooktoLibrary function

--- learning/odin/projects/library/library.js
+++ learning/odin/projects/library/library.js
@@ -9,54 +9,86 @@ function Book(title, author, date) {
   this.date = date;
 }
 
-bookInput.addEventListener("click", (event) => {
-  event.preventDefault();
-  let titleInput = document.querySelector("#title-input");
-  let authorInput = document.querySelector("#author-input");
-  let dateInput = document.querySelector("#date-input");
+function addBooktoLibrary() {
+  for (i = 0; i < myLibrary.length; i++) {
+    let div = document.createElement("div");
+    div.setAttribute("data-index", i);
+    let book = {
+      title: document.createElement("div"),
+      author: document.createElement("div"),
+      date: document.createElement("div"),
+      delBtn: document.createElement("button"),
+    };
+    book.title.innerText = `Title: ${book.title}`;
+    book.title.author = `Author: ${book.author}`;
+    book.title.date = `Date: ${book.date}`;
 
-  title = titleInput.value;
-  author = authorInput.value;
-  date = dateInput.value;
+    bookList.appendChild(div);
+    console.log(myLibrary[i]);
 
-  if (title === "" || author === "" || date === "") {
-    alert("Please fill out the required fields");
-  } else {
-    const book = new Book(title, author, date);
-    myLibrary.push(book);
-    displayBooks(book);
-    console.log(myLibrary);
+    for (const e in book) {
+      div.appendChild(book[e]);
+    }
+
+    btn.addEventListener("click", () => {
+      // bookCard.remove();
+      console.log("remove button clicked");
+    });
   }
-});
-
-function displayBooks(book) {
-  let div = document.createElement("div");
-  let bookInfo = document.createElement("div");
-  let bookTitle = document.createElement("p");
-  let bookAuthor = document.createElement("p");
-  let bookDate = document.createElement("p");
-  let btn = document.createElement("button");
-
-  div.classList.add("bookCard");
-  bookInfo.classList.add("bookInfo");
-  bookTitle.classList.add("bookTitle");
-  bookAuthor.classList.add("bookAuthor");
-  bookDate.classList.add("bookDate");
-  btn.classList.add("removeBtn");
-  btn.setAttribute("title", "Remove Book");
-
-  bookTitle.textContent = `Title: ${book.title}`;
-  bookAuthor.textContent = `Author: ${book.author}`;
-  bookDate.textContent = `Date: ${book.date}`;
-  btn.textContent = "Remove";
-
-  bookList.appendChild(div);
-  div.appendChild(bookInfo);
-  bookInfo.append(bookTitle, bookAuthor, bookDate);
-  div.appendChild(btn);
-
-  btn.addEventListener("click", () => {
-    // bookCard.remove();
-    console.log("remove button clicked");
-  });
 }
+
+addBooktoLibrary();
+
+// function addBooktoLibrary() {
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     let titleInput = document.querySelector("#title-input");
+//     let authorInput = document.querySelector("#author-input");
+//     let dateInput = document.querySelector("#date-input");
+
+//     title = titleInput.value;
+//     author = authorInput.value;
+//     date = dateInput.value;
+
+//     const book = new Book(title, author, date);
+//     myLibrary.push(book);
+//     displayBooks(book);
+//     console.log(myLibrary);
+//   }
+// }
+
+// function displayBooks(book) {
+//   let div = document.createElement("div");
+//   let bookInfo = document.createElement("div");
+//   let bookTitle = document.createElement("p");
+//   let bookAuthor = document.createElement("p");
+//   let bookDate = document.createElement("p");
+//   let btn = document.createElement("button");
+
+//   div.classList.add("bookCard");
+//   bookInfo.classList.add("bookInfo");
+//   bookTitle.classList.add("bookTitle");
+//   bookAuthor.classList.add("bookAuthor");
+//   bookDate.classList.add("bookDate");
+//   btn.classList.add("removeBtn");
+//   btn.setAttribute("title", "Remove Book");
+
+//   bookTitle.textContent = `Title: ${book.title}`;
+//   bookAuthor.textContent = `Author: ${book.author}`;
+//   bookDate.textContent = `Date: ${book.date}`;
+//   btn.textContent = "Remove";
+
+//   bookList.appendChild(div);
+//   div.appendChild(bookInfo);
+//   bookInfo.append(bookTitle, bookAuthor, bookDate);
+//   div.appendChild(btn);
+
+//   btn.addEventListener("click", () => {
+//     // bookCard.remove();
+//     console.log("remove button clicked");
+//   });
+// }
+
+bookInput.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBooktoLibrary();
+});
