c65347d465245108994681a235847a1bebab7de8
Eric R <erroeters@gmail.com>
2024-02-23 15:41:09 -0500

    Started js section of code along for tic tac toe game

--- learning/odin/projects/tic-tac-toe/tic-tac-toe.js
+++ learning/odin/projects/tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,23 @@
 // https://www.youtube.com/watch?v=kVE4xX-OkJo
+
+const Gameboard = (() => {
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+  const render = () => {
+    let boardHTML = "";
+    gameboard.forEach((square, index) => {
+      boardHTML += `<div class="square" id=square-${index}">${square}</div>`;
+    });
+  };
+  document.querySelector("#gameboard").innerHTML = boardHTML;
+
+  return {
+    render,
+  };
+})();
+
+const startBtn = document.querySelector(".startBtn");
+
+startBtn.addEventListener("click", () => {
+  //   Game.start();
+});
