dd7e588ead963331118282f5c3331fbda744343b
Eric R <erroeters@gmail.com>
2024-03-29 16:38:23 -0400

    Start work on animation exercise 3

--- learning/odin/css/animation/exercises/animation-exercise.js
+++ learning/odin/css/animation/exercises/animation-exercise.js
@@ -1,5 +1,7 @@
 const openButton = document.querySelector(".trigger-modal");
 const closeButton = document.querySelector(".close-modal");
+const menuBtn = document.querySelector(".menu-title");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
 function toggleModal() {
   const modalDiv = document.querySelector(".popup-modal");
@@ -8,5 +10,14 @@ function toggleModal() {
   backdrop.classList.toggle("show");
 }
 
+menuBtn.addEventListener("click", () => {
+  if (dropdownMenu.style.display == "none") {
+    dropdownMenu.style.display = "block";
+  } else {
+    dropdownMenu.style.display = "none";
+  }
+});
+
 openButton.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
+// menuBtn.addEventListener("click", toggleDropdown);
