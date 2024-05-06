e8b60dfa5457e76dfebf5955ba0030304024feb7
Eric R <erroeters@gmail.com>
2024-05-06 16:08:56 -0400

    Add python test files

---
+++ learning/udemy/python/test.py
@@ -0,0 +1,12 @@
+# this program says hello and asks for my name
+
+print('Hello World')
+print("what is your name?") #ask for their name
+myName = input() 
+print('it is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+
+print('What is your age?') #ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
